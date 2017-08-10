d60a52970321bf915d5133f4821f9c9de65580dd
App Academy Student <student@appacademy.io>
2017-08-09 17:16:52 -0700

    add Card class, Card spec, basic Deck Class & Deck spec

---
+++ poker/lib/deck.rb
@@ -0,0 +1,23 @@
+require_relative "card"
+
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = make_deck
+  end
+
+  def make_deck
+    suits = [:clubs, :spades, :hearts, :diamonds]
+    faces = [:"2", :"3", :"4", :"5", :"6", :"7", :"8", :"9", :"10", :jack, :queen, :king, :ace]
+
+    deck = []
+    suits.each do |suit|
+      faces.each do |face|
+        deck << Card.new(suit, face)
+      end
+    end
+
+    deck
+  end
+end
---
+++ poker/spec/deck_spec.rb
@@ -0,0 +1,24 @@
+require 'deck'
+
+describe 'Deck' do
+  subject(:deck) { Deck.new }
+  let(:card) { double(:Card) }
+
+  it "initializes with 52 cards" do
+    expect(deck.cards.length).to eq(52)
+  end
+
+  it "contains card objects" do
+    expect(deck.cards.sample).to be_a(Card)
+  end
+
+  it "contains four of a kind" do
+
+    four_kings = deck.cards.select do |card|
+      card.face == :king
+    end
+
+    expect(four_kings.length).to eq(4)
+  end
+
+end
