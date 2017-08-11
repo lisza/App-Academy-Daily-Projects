ac2080fa39af5a1bc825ed59a4c605e36da24873
App Academy Student <student@appacademy.io>
2017-08-10 17:19:28 -0700

    actually finishes anagrams exercise

--- anagrams.rb
+++ anagrams.rb
@@ -18,7 +18,7 @@ end
 ############################
 
 
-def anagrams?(str1, str2)
+def first_anagram?(str1, str2)
   # time complexity O(n! + n)
   # space complexity O(n!)
   all_permutations = permutations(str1)
@@ -38,4 +38,50 @@ def permutations(str)
     end
   end
   result
+end
+
+def second_anagram?(str1, str2)
+  # linear time complexity
+  # constant space complexity
+
+  str1_copy = str1.dup
+  str2_copy = str2.dup
+
+  str1.each_char do |ch| # loop through string 1
+    char_idx = str2_copy.index(ch)
+
+    if char_idx
+      str2_copy[char_idx] = ""
+      str1_copy[str1_copy.index(ch)] = ""
+    end
+
+  end
+
+  p str1_copy
+  p str2_copy
+
+  str2_copy.empty? && str1_copy.empty?
+end
+
+def third_anagram?(str1, str2)
+  # time complexity O(n^2)
+  # space complexity O(1)
+  str1.chars.sort == str2.chars.sort
+end
+
+def fourth_anagram?(str1, str2)
+  # time complexity O(n)
+  # space complexity O(n)
+  str1_hash = Hash.new(0)
+  str2_hash = Hash.new(0)
+
+  str1.chars.each do |ch|
+    str1_hash[ch] += 1
+  end
+
+  str2.chars.each do |ch|
+    str2_hash[ch] += 1
+  end
+
+  str1_hash == str2_hash
 end
